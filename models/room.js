c68019d6ee24bbd31fd583aef5729f14cc9dad26
Briyan Adityatama <briyansykes@yahoo.com>
2018-11-11 22:47:03 +0700

    implement room.js models DB sequelize ORM MySQL

---
+++ models/room.js
@@ -0,0 +1,37 @@
+"use strict";
+
+module.exports = function(sequelize, DataTypes) {
+  var Room = sequelize.define(
+    "room",
+    {
+      roomId: {
+        type: DataTypes.INTEGER,
+        autoIncrement: true,
+        allowNull: false,
+        unique: true
+      },
+      roomName: {
+        type: DataTypes.STRING(255)
+      },
+      roomType: {
+        type: DataTypes.STRING(255)
+      },
+      description: {
+        type: DataTypes.STRING
+      },
+      imageURL: {
+        type: DataTypes.STRING(2048)
+      },
+      quantity: {
+        type: DataTypes.INTEGER(3)
+      },
+      price: {
+        type: DataTypes.STRING(255)
+      }
+    },
+    {
+      freezeTableName: true
+    }
+  );
+  return Room;
+};
