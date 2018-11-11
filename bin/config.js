006a8a24a92a0c35041a3478efa0ba0dc62d4cd7
Briyan Adityatama <briyansykes@yahoo.com>
2018-11-11 22:12:25 +0700

    implement config for mysql connection

---
+++ bin/config.js
@@ -0,0 +1,7 @@
+var config = {
+  // Set your MySQL Connection Databae (fill with your own account e.g :
+  // mysql: your username, password: your password, mydb: your local database)
+  MYSQL_URL: "mysql://mysql:password@localhost:3306/mydb"
+};
+
+module.exports = config;
