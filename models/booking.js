f07c2b6981213f5e0fd0ba6500bcfc34a2f662f2
Briyan Adityatama <briyansykes@yahoo.com>
2018-11-11 23:04:29 +0700

    implement booking.js models DB sequelize MySQL ORM

---
+++ models/booking.js
@@ -0,0 +1,47 @@
+"use strict";
+
+module.exports = function(sequelize, DataTypes) {
+  var Booking = sequelize.define(
+    "booking",
+    {
+      bookingId: {
+        type: DataTypes.INTEGER,
+        autoIncrement: true,
+        allowNull: false,
+        unique: true
+      },
+      bookingDate: {
+        type: DataTypes.DATE,
+        defaultValue: DataTypes.NOW
+      },
+      checkInDate: {
+        type: DataTypes.DATE
+      },
+      checkOutDate: {
+        type: DataTypes.DATE
+      },
+      name: {
+        type: DataTypes.STRING(255)
+      },
+      type: {
+        type: DataTypes.STRING(255)
+      },
+      description: {
+        type: DataTypes.TEXT
+      },
+      quantity: {
+        type: DataTypes.INTEGER(3)
+      },
+      price: {
+        type: DataTypes.STRING(255)
+      },
+      payment: {
+        type: DataTypes.ENUM("transfer", "cash", "gateway")
+      }
+    },
+    {
+      freezeTableName: true
+    }
+  );
+  return Booking;
+};
