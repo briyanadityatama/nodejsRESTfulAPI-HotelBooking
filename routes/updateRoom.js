b96294ddde947674ef0eb497353428fe29b1acc3
Briyan Adityatama <briyansykes@yahoo.com>
2018-11-12 22:09:43 +0700

    fix updateRoom.js

--- routes/updateRoom.js
+++ routes/updateRoom.js
@@ -2,27 +2,25 @@ var express = require("express");
 var router = express.Router();
 var models = require("../models");
 
-router.post("/", function(req, res, next) {
+// Update Room data findBy id
+router.put("/", function(req, res, next) {
   try {
-    models.room
-      .update(
-        {
-          roomName: req.body.roomName,
-          description: req.body.description,
-          imageURL: req.body.imageURL,
-          quantity: req.body.quantity,
-          price: req.body.price
-        },
-        {
-          where: {
-            roomId: req.body.roomId,
-            roomType: req.body.roomType
-          }
+    models.Room.update(
+      {
+        roomName: req.body.roomName,
+        description: req.body.description,
+        imageURL: req.body.imageURL,
+        quantity: req.body.quantity,
+        price: req.body.price
+      },
+      {
+        where: {
+          id: req.body.id
         }
-      )
-      .then(function() {
-        res.redirect("/update-room");
-      });
+      }
+    ).then(function() {
+      res.redirect("/update-room/:roomId");
+    });
   } catch (e) {
     console.log(e.toString());
   }
