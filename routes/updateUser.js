2d068d4626952996978614d8002136c53356846d
Briyan Adityatama <briyansykes@yahoo.com>
2018-11-12 22:11:54 +0700

    fix updateUser.js

--- routes/updateUser.js
+++ routes/updateUser.js
@@ -2,28 +2,26 @@ var express = require("express");
 var router = express.Router();
 var models = require("../models");
 
-router.post("/", function(req, res, next) {
+// Update User data findBy emails
+router.put("/", function(req, res, next) {
   try {
-    models.user
-      .update(
-        {
-          firstName: req.body.firstName,
-          lastName: req.body.lastName,
-          phone: req.body.phone,
-          address: req.body.address,
-          country: req.body.country,
-          state: req.body.state
-        },
-        {
-          where: {
-            userId: req.body.userId,
-            email: req.body.email
-          }
+    models.User.update(
+      {
+        firstName: req.body.firstName,
+        lastName: req.body.lastName,
+        phone: req.body.phone,
+        address: req.body.address,
+        country: req.body.country,
+        state: req.body.state
+      },
+      {
+        where: {
+          email: req.body.email
         }
-      )
-      .then(function() {
-        res.redirect("/update-user");
-      });
+      }
+    ).then(function() {
+      res.redirect("/update-user/:email");
+    });
   } catch (e) {
     console.log(e.toString());
   }
