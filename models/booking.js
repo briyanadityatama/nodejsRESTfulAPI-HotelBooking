e72b3c4924b10ea88e5fdb14660c54decaddb3c6
Briyan Adityatama <briyansykes@yahoo.com>
2018-11-12 21:17:12 +0700

    fix Booking.js models

--- models/booking.js
+++ models/booking.js
@@ -1,61 +1,59 @@
 "use strict";
 
 module.exports = function(sequelize, DataTypes) {
-  var Booking = sequelize.define(
-    "Booking",
-    {
-      // bookingId: {
-      //   type: DataTypes.INTEGER,
-      //   autoIncrement: true,
-      //   allowNull: false,
-      //   unique: true
-      // },
-      bookingDate: {
-        type: DataTypes.DATE,
-        defaultValue: DataTypes.NOW
-      },
-      checkInDate: {
-        type: DataTypes.DATE
-      },
-      checkOutDate: {
-        type: DataTypes.DATE
-      },
-      name: {
-        type: DataTypes.STRING(255)
-      },
-      phone: {
-        type: DataTypes.INTEGER(20)
-      },
-      state: {
-        type: DataTypes.STRING(255)
-      },
-      email: {
-        type: DataTypes.STRING(255),
-        allowNull: false,
-        validate: { isEmail: true },
-        unique: true
-      },
-      type: {
-        type: DataTypes.STRING(255)
-      },
-      description: {
-        type: DataTypes.TEXT
-      },
-      quantity: {
-        type: DataTypes.INTEGER(3)
-      },
-      price: {
-        type: DataTypes.STRING(255)
-      },
-      payment: {
-        type: DataTypes.ENUM("transfer", "cash", "gateway")
-      }
+  var Booking = sequelize.define("Booking", {
+    // bookingId: {
+    //   type: DataTypes.INTEGER,
+    //   autoIncrement: true,
+    //   allowNull: false,
+    //   unique: true
+    // },
+    bookingDate: {
+      type: DataTypes.DATE,
+      defaultValue: DataTypes.NOW
     },
-    (Booking.associate = function(models) {
-      // Booking belongsTo User
-      Booking.belongsTo(models.User, { foreignKey: "userId" });
-      Booking.hasMany(models.Room);
-    })
-  );
+    checkInDate: {
+      type: DataTypes.DATE
+    },
+    checkOutDate: {
+      type: DataTypes.DATE
+    },
+    name: {
+      type: DataTypes.STRING(255)
+    },
+    phone: {
+      type: DataTypes.INTEGER(20)
+    },
+    state: {
+      type: DataTypes.STRING(255)
+    },
+    email: {
+      type: DataTypes.STRING(255),
+      allowNull: false,
+      validate: { isEmail: true },
+      unique: true
+    },
+    type: {
+      type: DataTypes.STRING(255)
+    },
+    description: {
+      type: DataTypes.TEXT
+    },
+    quantity: {
+      type: DataTypes.INTEGER(3)
+    },
+    price: {
+      type: DataTypes.STRING(255)
+    },
+    payment: {
+      type: DataTypes.ENUM("transfer", "cash", "gateway")
+    }
+  });
+
+  Booking.associate = function(models) {
+    // Booking belongsTo User
+    Booking.belongsTo(models.User, { foreignKey: "userId" });
+    Booking.hasMany(models.Room);
+  };
   return Booking;
 };
