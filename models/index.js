c499b7824f2f1b2556d9afe6421caaba64ce9414
Briyan Adityatama <briyansykes@yahoo.com>
2018-11-12 23:08:48 +0700

    fix index.js models

--- models/index.js
+++ models/index.js
@@ -3,22 +3,38 @@
 var fs = require("fs");
 var path = require("path");
 var Sequelize = require("sequelize");
-var config = require("../bin/config");
-
-var sequelize = new Sequelize(config.MYSQL_URL);
+var basename = path.basename(__filename);
+var env = process.env.NODE_ENV || "development";
+var config = require(__dirname + "/../config/config.json")[env];
+// var configjs  = require("../bin/config");
 var db = {};
 
+// var sequelize = new Sequelize(configjs.MYSQL_URL);
+
+if (config.use_env_variable) {
+  var sequelize = new Sequelize(process.env[config.use_env_variable], config);
+} else {
+  var sequelize = new Sequelize(
+    config.database,
+    config.username,
+    config.password,
+    config
+  );
+}
+
 fs.readdirSync(__dirname)
-  .filter(function(file) {
-    return file.indexOf(".") !== 0 && file !== "index.js";
+  .filter(file => {
+    return (
+      file.indexOf(".") !== 0 && file !== basename && file.slice(-3) === ".js"
+    );
   })
-  .forEach(function(file) {
-    var model = sequelize.import(path.join(__dirname, file));
+  .forEach(file => {
+    var model = sequelize["import"](path.join(__dirname, file));
     db[model.name] = model;
   });
 
-Object.keys(db).forEach(function(modelName) {
-  if ("associate" in db[modelName]) {
+Object.keys(db).forEach(modelName => {
+  if (db[modelName].associate) {
     db[modelName].associate(db);
   }
 });
