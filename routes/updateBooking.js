50f832b6c6597ca0a411c868c37e39633f4a82cf
Briyan Adityatama <briyansykes@yahoo.com>
2018-11-12 22:07:41 +0700

    fix updateBooking.js

--- routes/updateBooking.js
+++ routes/updateBooking.js
@@ -2,33 +2,31 @@ var express = require("express");
 var router = express.Router();
 var models = require("../models");
 
-router.post("/", function(req, res, next) {
+// Update Booking data findBy emails
+router.put("/", function(req, res, next) {
   try {
-    if (checkInDate.req.checkInDate == Date.now) {
+    if (req.body.checkInDate == Date.now) {
       throw console.error(
         "Your Check In Date is too close, you can't update your Booking! "
       );
     }
-    models.booking
-      .update(
-        {
-          checkInDate: req.body.checkInDate,
-          checkOutDate: req.body.checkOutDate,
-          name: req.body.name,
-          phone: req.body.phone,
-          description: req.body.description,
-          quantity: req.body.quantity
-        },
-        {
-          where: {
-            bookingId: req.body.bookingId,
-            email: req.body.email
-          }
+    models.Booking.update(
+      {
+        checkInDate: req.body.checkInDate,
+        checkOutDate: req.body.checkOutDate,
+        name: req.body.name,
+        phone: req.body.phone,
+        description: req.body.description,
+        quantity: req.body.quantity
+      },
+      {
+        where: {
+          email: req.body.email
         }
-      )
-      .then(function() {
-        res.redirect("/update-booking");
-      });
+      }
+    ).then(function() {
+      res.redirect("/update-booking/:email");
+    });
   } catch (e) {
     console.log(e.toString());
   }
