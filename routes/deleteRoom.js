d98c582f1ed3f70e81f5b04d253be84e69b151f2
Briyan Adityatama <briyansykes@yahoo.com>
2018-11-12 21:47:46 +0700

    fix deleteRoom.js

--- routes/deleteRoom.js
+++ routes/deleteRoom.js
@@ -4,13 +4,13 @@ var models = require("../models");
 
 router.get("/", function(req, res, next) {
   try {
-    models.room
-      .destroy({
-        where: { roomId: req.query.roomId }
-      })
-      .then(function() {
-        res.redirect("/delete-room/:id");
-      });
+    models.Room.destroy({
+      where: {
+        id: req.query.id
+      }
+    }).then(function() {
+      res.redirect("/delete-room/:id");
+    });
   } catch (e) {
     console.log(e.toString());
   }
