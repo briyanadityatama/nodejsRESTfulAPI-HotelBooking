168b501c8b38bd86446777ae201c04007cedb52d
Briyan Adityatama <briyansykes@yahoo.com>
2018-11-13 12:31:17 +0700

    fix migrations create-user sequelize

--- migrations/20181113002302-create-user.js
+++ migrations/20181113002302-create-user.js
@@ -9,55 +9,55 @@ module.exports = {
         type: Sequelize.INTEGER
       },
       identityId: {
-        type: DataTypes.INTEGER,
+        type: Sequelize.INTEGER,
         autoIncrement: true,
         primaryKey: true
       },
       identityType: {
-        type: DataTypes.ENUM("ktp", "pasport", "sim")
+        type: Sequelize.ENUM("ktp", "pasport", "sim")
       },
       firstName: {
-        type: DataTypes.STRING(255),
+        type: Sequelize.STRING(255),
         allowNull: false,
         unique: "compositeIndex"
       },
       lastName: {
-        type: DataTypes.STRING(255),
+        type: Sequelize.STRING(255),
         unique: "compositeIndex"
       },
-      password: { type: DataTypes.STRING(100) },
+      password: { type: Sequelize.STRING(100) },
 
       email: {
-        type: DataTypes.STRING(255),
+        type: Sequelize.STRING(255),
         allowNull: false,
         validate: { isEmail: true },
         unique: true
       },
       phone: {
-        type: DataTypes.INTEGER(20)
+        type: Sequelize.INTEGER(20)
       },
       gender: {
-        type: DataTypes.ENUM("male", "female")
+        type: Sequelize.ENUM("male", "female")
       },
       age: {
-        type: DataTypes.INTEGER(3),
+        type: Sequelize.INTEGER(3),
         validate: { max: 999, min: 18 }
       },
       address: {
-        type: DataTypes.TEXT
+        type: Sequelize.TEXT
       },
       country: {
-        type: DataTypes.STRING(255)
+        type: Sequelize.STRING(255)
       },
       state: {
-        type: DataTypes.STRING(255)
+        type: Sequelize.STRING(255)
       },
       dateCheckIn: {
-        type: DataTypes.DATE,
-        defaultValue: DataTypes.NOW
+        type: Sequelize.DATE,
+        defaultValue: Sequelize.NOW
       },
       dateCheckOut: {
-        type: DataTypes.DATE
+        type: Sequelize.DATE
       },
       createdAt: {
         allowNull: false,
